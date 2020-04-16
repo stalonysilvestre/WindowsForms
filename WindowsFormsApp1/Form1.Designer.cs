994dbbe41310d2bb5b2abe4b72f8eed21889889c
Silvestre Stalony <Silvestre Stalony@STALONY-PC>
2020-04-16 17:19:20 -0300

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_executar = new System.Windows.Forms.Button();
+            this.caixa_de_texto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_executar
+            // 
+            this.btn_executar.Location = new System.Drawing.Point(646, 375);
+            this.btn_executar.Name = "btn_executar";
+            this.btn_executar.Size = new System.Drawing.Size(75, 23);
+            this.btn_executar.TabIndex = 0;
+            this.btn_executar.Text = "Executar";
+            this.btn_executar.UseVisualStyleBackColor = true;
+            this.btn_executar.Click += new System.EventHandler(this.Btn_executar_Click);
+            // 
+            // caixa_de_texto
+            // 
+            this.caixa_de_texto.Location = new System.Drawing.Point(69, 160);
+            this.caixa_de_texto.Name = "caixa_de_texto";
+            this.caixa_de_texto.Size = new System.Drawing.Size(652, 40);
+            this.caixa_de_texto.TabIndex = 1;
+            this.caixa_de_texto.Text = "Texto de exemplo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.caixa_de_texto);
+            this.Controls.Add(this.btn_executar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_executar;
+        private System.Windows.Forms.Label caixa_de_texto;
+    }
+}
+
